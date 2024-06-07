482b8b22d8780cef942787361afa8d923da8e105
PeterBing <2475013057@qq.com>
2024-06-07 21:32:35 +0800

    温湿度传感器代码

---
+++ sht20.c
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2023 Beijing HuaQing YuanJian Education Technology Co., Ltd
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "ohos_init.h"
+#include "cmsis_os2.h"
+
+#include "hi_io.h"
+#include "hi_gpio.h"
+#include "hi_i2c.h"
+#include "hi_errno.h"
+
+#include "hal_bsp_structAll.h"
+#include "hal_bsp_sht20.h"
+#include "hal_bsp_aw2013.h"
+#include "hal_bsp_ap3216c.h"
+#include "hal_bsp_ssd1306.h"
+#include "hal_bsp_pcf8574.h"
+#include "hal_bsp_nfc.h"
+#include "hal_bsp_wifi.h"
+#include "wifi_device.h"
+#include "lwip/netifapi.h"
+#include "lwip/sockets.h"
+#include "lwip/api_shell.h"
+#define MS(sec) (sec*1000)
+#define SHT20_SLA_ADDR 0x80
+typedef enum IO_sw{
+    ON,//代表0
+    OFF,//代表1
+}io_sw;
+uint8_t iodata;
+
+
+//向传感器发送数据的函数
+void hqyj_sht20_write(uint8_t data)
+{
+    int ret=0;
+    hi_i2c_data i2c_data;
+    i2c_data.send_buf=&data;
+    i2c_data.send_len=sizeof(data);
+    ret=hi_i2c_write(HI_I2C_IDX_0,SHT20_SLA_ADDR,&i2c_data);
+    if(ret!=0)
+    {
+        printf("hi_i2c_read error ret:%#x\n",ret);
+    }
+}
+
+//传感器初始化
+void hqyj_sht20_init()
+{
+    //1.IO引脚功能的复用
+    hi_io_set_func(HI_IO_NAME_GPIO_9,HI_IO_FUNC_GPIO_9_I2C0_SCL);
+    hi_io_set_func(HI_IO_NAME_GPIO_10,HI_IO_FUNC_GPIO_10_I2C0_SDA);
+    //设置引脚上拉
+    hi_io_set_pull(HI_IO_NAME_GPIO_9,HI_IO_PULL_UP);
+    hi_io_set_func(HI_IO_NAME_GPIO_10,HI_IO_PULL_UP);
+    //i2c初始化
+    hi_i2c_init(HI_I2C_IDX_0,100000);
+    //向传感器发送0xFE进行软复位
+    hqyj_sht20_write(0xFE);
+    //休眠15ms等待传感器软复位完毕
+    usleep(MS(20));
+}
+
+//从sht20接收数据函数
+void hqyj_sht20_read(uint8_t *buf)
+{
+    int ret=0;
+    hi_i2c_data i2c_data;
+    i2c_data.receive_buf=buf;
+    i2c_data.receive_len=3;
+    ret=hi_i2c_read(HI_I2C_IDX_0,SHT20_I2C_ADDR,&i2c_data);
+    if(ret!=0)
+    {
+        printf("hi_i2c_read error ret:%#x\n",ret);
+    }
+}
+
+void hqyj_read_temp_humi(float *temp,float *humi)
+{
+    uint8_t rcv_buf[3]={0};
+    //1.发送测量温度指令
+    hqyj_sht20_write(0xF3);
+    //2.休眠等待测量完毕
+    usleep(MS(100));
+    //3.读取温度数据
+    hqyj_sht20_read(rcv_buf);
+    //对读到的数据进行解析
+    *temp=((((rcv_buf[0]<<8)|rcv_buf[1])/65536.0)*175.72)-46.85;
+    //湿度数据的获取
+    //1.发送测量湿度的指令
+    hqyj_sht20_write(0xF5);
+    //2.休眠等待湿度测量完毕
+    usleep(MS(40));
+    //3.读取湿度数据
+    hqyj_sht20_read(rcv_buf);
+    //4.对接收到的湿度的数据进行解析
+    *humi=((((rcv_buf[0]<<8)|rcv_buf[1])/65536.0)*125.0)-6.0;
+}
+
+//控制蜂鸣器的函数
+void hqyj_control_beep(int sw)
+{
+     if(sw==OFF)
+    {
+        //2.控制蜂鸣器关
+         iodata=iodata |(1<<1);
+    }else if(sw==ON)
+    {
+        //2.控制蜂鸣器开
+        iodata=iodata &(~(1<<1));
+    }
+    PCF8574_Write(iodata);
+}
+
+void hqyj_control_led(io_sw sw)//p3口接低电平导通
+{
+    if(sw==OFF)
+    {
+        //2.控制灯亮灭
+        iodata=iodata |(1<<2);        
+    }else if(sw==ON)
+    {
+        //2.控制灯亮
+        iodata=iodata &(~(1<<2));        
+    }
+    PCF8574_Write(iodata);
+}
+
+//温度超过阈值，蜂鸣器响,湿度超过阈值，LED灯亮
+void hqyj_temp_thr()
+{
+    float temp=0,humi=0;
+    while(1)
+    {   
+        hqyj_read_temp_humi(&temp,&humi);
+        //%.2f是控制打印格式，只打印小数点后两位
+        printf("temp=%.2f,humi=%.2f\n",temp,humi);
+        usleep(MS(1000));
+        if(temp<31||temp>33)
+        {
+            iodata=iodata &(~(1<<1));
+            PCF8574_Write(iodata);
+        }else{
+            iodata=iodata |(1<<1);
+            PCF8574_Write(iodata);
+        }
+        if(humi<40||humi>60)
+        {
+            iodata=iodata &(~(1<<2));  
+            PCF8574_Write(iodata);
+        }else{
+            iodata=iodata |(1<<2);  
+            PCF8574_Write(iodata);
+        }
+    }
+}
+
+void mytest(void)
+{   
+    
+    //1.传感器初始化
+    PCF8574_Init();
+    hqyj_sht20_init();
+    hqyj_temp_thr();
+    while(1)
+    {
+        usleep(50000);
+    }
+   
+}
+SYS_RUN(mytest);
--- test6_3.cpp
+++ test6_3.cpp
@@ -1,81 +1,176 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "ohos_init.h"
+#include "cmsis_os2.h"
+
+#include "hi_io.h"
+#include "hi_gpio.h"
+#include "hi_i2c.h"
+#include "hi_errno.h"
+
+#include "hal_bsp_structAll.h"
+#include "hal_bsp_sht20.h"
+#include "hal_bsp_aw2013.h"
+#include "hal_bsp_ap3216c.h"
+#include "hal_bsp_ssd1306.h"
+#include "hal_bsp_pcf8574.h"
+#include "hal_bsp_nfc.h"
+#include "hal_bsp_wifi.h"
+#include "wifi_device.h"
+#include "lwip/netifapi.h"
+#include "lwip/sockets.h"
+#include "lwip/api_shell.h"
+#define MS(sec) (sec*1000)
+#define SHT20_SLA_ADDR 0x80
+typedef enum IO_sw {
+    ON,//代表0
+    OFF,//代表1
+}io_sw;
+uint8_t iodata;
 
 
-//UART串口发送信息
-void UART_Init(void)
+//向传感器发送数据的函数
+void hqyj_sht20_write(uint8_t data)
 {
-	//1.将GPA1_0和GPA1_1设置成UART2的接收引脚和发送引脚 GPA1CON[7:0]
-	GPA1.CON = GPA1.CON & (~(0xFF << 0)) | (0x22 << 0);
-	//2.设置UART2的帧格式 8位数据位 1位停止位 无校验位 正常模式 ULCON2
-	UART2.ULCON2 = UART2.ULCON2 & (~(0x7F << 0)) | (0x3 << 0);
-	//设置UART2的接收和发送模式为轮询模式 UCON2[3:0]
-	UART2.UCON2 = UART2.UCON2 & (~(0xF << 0)) | (0x5 << 0);
-	//设置UART2的波特率为115200  UBRDIV2/UFRACVAL2
-	UART2.UBRDIV2 = 53;
-	UART2.UFRACVAL2 = 4;
+    int ret = 0;
+    hi_i2c_data i2c_data;
+    i2c_data.send_buf = &data;
+    i2c_data.send_len = sizeof(data);
+    ret = hi_i2c_write(HI_I2C_IDX_0, SHT20_SLA_ADDR, &i2c_data);
+    if (ret != 0)
+    {
+        printf("hi_i2c_read error ret:%#x\n", ret);
+    }
 }
 
-
-char UART_Rec_Byte(void)
+//传感器初始化
+void hqyj_sht20_init()
 {
-	char Dat = 0;
-	//判断接收寄存器是否接收到数据
-	if (UART.UTRSTAT2 & 1)
-	{
-		Dat = UART.URXH2;
-		return Dat;
-	}
-	else
-	{
-		return 0;
-	}
-
-void UART_Send_Str(char* pstr)
-{
-	////等待发送寄存器寄存器为空
-	//while (!(UART2.UTRSTAT2 & (1 << 1)));
-	////将发送的数据写入发送寄存器URXH2
-	//UART2.UTXH2 = Dat;
-
-	while (*pstr != '\0')
-		UART_Send_Byte(*pstr++);
+    //1.IO引脚功能的复用
+    hi_io_set_func(HI_IO_NAME_GPIO_9, HI_IO_FUNC_GPIO_9_I2C0_SCL);
+    hi_io_set_func(HI_IO_NAME_GPIO_10, HI_IO_FUNC_GPIO_10_I2C0_SDA);
+    //设置引脚上拉
+    hi_io_set_pull(HI_IO_NAME_GPIO_9, HI_IO_PULL_UP);
+    hi_io_set_func(HI_IO_NAME_GPIO_10, HI_IO_PULL_UP);
+    //i2c初始化
+    hi_i2c_init(HI_I2C_IDX_0, 100000);
+    //向传感器发送0xFE进行软复位
+    hqyj_sht20_write(0xFE);
+    //休眠15ms等待传感器软复位完毕
+    usleep(MS(20));
 }
 
-char UART_Rec_Byte(void)
+//从sht20接收数据函数
+void hqyj_sht20_read(uint8_t* buf)
 {
-	char Dat = 0;
-	//判断接收器寄存器是否接收到数据
-	if (UART2.UTRSTAT2 & 1)
-	{
-		Dat = UART2.UARXH2;
-		return Dat;
-	}
-	else
-	{
-		return 0;
-	}
+    int ret = 0;
+    hi_i2c_data i2c_data;
+    i2c_data.receive_buf = buf;
+    i2c_data.receive_len = 3;
+    ret = hi_i2c_read(HI_I2C_IDX_0, SHT20_I2C_ADDR, &i2c_data);
+    if (ret != 0)
+    {
+        printf("hi_i2c_read error ret:%#x\n", ret);
+    }
 }
 
-int main()
+void hqyj_read_temp_humi(float* temp, float* humi)
 {
-	char RecDat = 0;
-	UART_Init();
-
-	while (1)
-	{
-		//RecDat = UART_Byte();
-		//if (RecDat == 0)
-		//{
-
-		//}
-		//else
-		//{
-		//	RecDat = RecDat + 1;
-		//	UART_Send_Byte(RecDat);
-
-		UART_Send_Str("Hello World\n");
-		}
-	}
-	return 0;
+    uint8_t rcv_buf[3] = { 0 };
+    //1.发送测量温度指令
+    hqyj_sht20_write(0xF3);
+    //2.休眠等待测量完毕
+    usleep(MS(100));
+    //3.读取温度数据
+    hqyj_sht20_read(rcv_buf);
+    //对读到的数据进行解析
+    *temp = ((((rcv_buf[0] << 8) | rcv_buf[1]) / 65536.0) * 175.72) - 46.85;
+    //湿度数据的获取
+    //1.发送测量湿度的指令
+    hqyj_sht20_write(0xF5);
+    //2.休眠等待湿度测量完毕
+    usleep(MS(40));
+    //3.读取湿度数据
+    hqyj_sht20_read(rcv_buf);
+    //4.对接收到的湿度的数据进行解析
+    *humi = ((((rcv_buf[0] << 8) | rcv_buf[1]) / 65536.0) * 125.0) - 6.0;
 }
+
+//控制蜂鸣器的函数
+void hqyj_control_beep(int sw)
+{
+    if (sw == OFF)
+    {
+        //2.控制蜂鸣器关
+        iodata = iodata | (1 << 1);
+    }
+    else if (sw == ON)
+    {
+        //2.控制蜂鸣器开
+        iodata = iodata & (~(1 << 1));
+    }
+    PCF8574_Write(iodata);
+}
+
+void hqyj_control_led(io_sw sw)//p3口接低电平导通
+{
+    if (sw == OFF)
+    {
+        //2.控制灯亮灭
+        iodata = iodata | (1 << 2);
+    }
+    else if (sw == ON)
+    {
+        //2.控制灯亮
+        iodata = iodata & (~(1 << 2));
+    }
+    PCF8574_Write(iodata);
+}
+
+//温度超过阈值，蜂鸣器响,湿度超过阈值，LED灯亮
+void hqyj_temp_thr()
+{
+    float temp = 0, humi = 0;
+    while (1)
+    {
+        hqyj_read_temp_humi(&temp, &humi);
+        //%.2f是控制打印格式，只打印小数点后两位
+        printf("temp=%.2f,humi=%.2f\n", temp, humi);
+        usleep(MS(1000));
+        if (temp < 31 || temp>33)
+        {
+            iodata = iodata & (~(1 << 1));
+            PCF8574_Write(iodata);
+        }
+        else {
+            iodata = iodata | (1 << 1);
+            PCF8574_Write(iodata);
+        }
+        if (humi < 40 || humi>60)
+        {
+            iodata = iodata & (~(1 << 2));
+            PCF8574_Write(iodata);
+        }
+        else {
+            iodata = iodata | (1 << 2);
+            PCF8574_Write(iodata);
+        }
+    }
+}
+
+void mytest(void)
+{
+
+    //1.传感器初始化
+    PCF8574_Init();
+    hqyj_sht20_init();
+    hqyj_temp_thr();
+    while (1)
+    {
+        usleep(50000);
+    }
+
+}
+SYS_RUN(mytest);
